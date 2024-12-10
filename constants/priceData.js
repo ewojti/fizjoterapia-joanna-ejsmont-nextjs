7760c3e6c3627757d96e6c24e2441de2f87f8167
Wojtek E <wojejs@gmail.com>
2024-12-10 21:54:00 +0100

    change price

--- constants/priceData.js
+++ constants/priceData.js
@@ -2,25 +2,25 @@ const priceData = [
   {
     id: 1,
     name: "Konsultacja fizjoterapeutyczna",
-    cost: "-120 zł",
+    cost: "-140 zł",
     time: "1h",
   },
   {
     id: 2,
     name: "Kolejna wizyta",
-    cost: "-120 zł",
+    cost: "-140 zł",
     time: "45-50 min",
   },
   {
     id: 3,
-    name: "Rehabilitacja domowa",
-    cost: "-140 zł",
+    name: "Rehabilitacja domowa - obecnie usługa niedostępna",
+    cost: "-160 zł",
     time: '- 45min'
   },
   {
     id: 4,
     name: "Taping",
-    cost: "-30 zł",
+    cost: "-40 zł",
     time: 'do 10 min' },
 ];
 
